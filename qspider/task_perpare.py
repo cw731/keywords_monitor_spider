04b80c804fe97acc7fba00b3f36039048d226315
qjun <639178489@qq.com>
2019-06-25 13:15:46 +0800

    完成task_perpare.py的拆解任务

--- qspider/task_perpare.py
+++ qspider/task_perpare.py
@@ -7,10 +7,26 @@
 # 获取任务目录
 import json
 import os
-
+from functools import wraps
 
 from qspider.settings import TASK_FILE_DIR
 from qspider.status.status_data_ctl import StatusAndData
+
+qq = StatusAndData()
+
+
+# def task_status(function):
+#     @wraps(function)
+#     def wrapper(*args, **kwargs):
+#         data = None
+#         try:
+#             data = function(*args, **kwargs)
+#         except:
+#             qq.code = qq.code = '4' + code[1:]
+#         return data
+#
+#     return wrapper
+
 
 
 def get_file_path(qq, code):
@@ -42,7 +58,28 @@ def get_file2json(qq, code):
 # 拆解任务
 def mince_task(qq, code):
     qq.code = code
-    pass
+    task_list = []
+    count = 0
+    for data in qq.data:
+        for url in data['urls']:
+            count += 1
+            task_list.append({
+                'task_id': count,
+                'url': url,
+                "name": '{}_{}'.format(data['name'], str(count)),
+                "parent_name": data['name'],
+                "info": data['info'],
+                "rule": {
+                    "rule_type": data['rule']['rule_type'],
+                    "rule_data": data['rule']['rule_data']
+                },
+                "data": {
+                    "data_type": data['data']['data_type'],
+                    "data": data['data']['data']
+                },
+            })
+    qq.data = task_list
+    return None
 
 
 # 任务准备控制
@@ -60,9 +97,9 @@ def task_perpare(qq):
     return None
 
 
+
 if __name__ == '__main__':
     qq = StatusAndData()
     task_perpare(qq)
-
 
 # ['test_task2.txt', 'test_task1.txt']
